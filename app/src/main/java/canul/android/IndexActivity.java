ffea318fd4f364ae557ee8cb25769385fd294523
vouillon thibaud <vouillon.thibaud@gmail.com>
2015-10-09 15:27:28 +0200

    parser + listview alpha

--- app/src/main/java/canul/android/IndexActivity.java
+++ app/src/main/java/canul/android/IndexActivity.java
@@ -2,12 +2,17 @@ package canul.android;
 
 import android.app.ListActivity;
 import android.content.Context;
+import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.squareup.okhttp.OkHttpClient;
@@ -19,6 +24,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class IndexActivity extends ListActivity {
 
@@ -27,18 +34,30 @@ public class IndexActivity extends ListActivity {
     private ProgressBar progressBar;
     String stringUrlArticles = "http://dev.canul.fr/api/articles";
 
-
+    ListView list;
     private static final String TAG_TITLE = "title";
     private static final String TAG_CONTENT = "content";
     private static final String TAG_AUTHOR = "author";
 
-
+    ArrayList<HashMap<String, String>> oslist = new ArrayList<HashMap<String, String>>();
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_index);
-        textView = (TextView) findViewById(R.id.myText);
+
+        oslist = new ArrayList<HashMap<String, String>>();
         progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+
+
+
+
+
+
+
+
+
+
+
 
         ConnectivityManager connMgr = (ConnectivityManager)
                 getSystemService(Context.CONNECTIVITY_SERVICE);
@@ -50,6 +69,29 @@ public class IndexActivity extends ListActivity {
             textView.setText("No network connection available.");
         }
 
+        ListView lv = getListView();
+
+        // Listview on item click listener
+        lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // getting values from selected ListItem
+                String title = ((TextView) view.findViewById(R.id.title))
+                        .getText().toString();
+                String content = ((TextView) view.findViewById(R.id.content))
+                        .getText().toString();
+
+                // Starting single contact activity
+                Intent in = new Intent(getApplicationContext(),SingleArticleActivity.class);
+                in.putExtra(TAG_TITLE, title);
+                in.putExtra(TAG_CONTENT, content);
+
+                startActivity(in);
+
+            }
+        });
 
     }
 
@@ -98,7 +140,7 @@ public class IndexActivity extends ListActivity {
         protected String doInBackground(String... urls) {
             GetJson example = new GetJson();
             String response = null;
-            String success=null;
+
             try {
 
                 response = example.run(urls[0]);
@@ -108,7 +150,23 @@ public class IndexActivity extends ListActivity {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-            jsonStr=response;
+
+
+
+            //intermediate = String.valueOf(articles.length());
+            intermediate=response;
+            //intermediate=success;
+
+            return response;
+        }
+
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            String success=null;
+            jsonStr=intermediate;
+
+
             if (jsonStr != null) {
                 try {
                     JSONObject jsonObj = new JSONObject(jsonStr);
@@ -122,30 +180,30 @@ public class IndexActivity extends ListActivity {
                         String content = c.getString(TAG_CONTENT);
                         String author = c.getString(TAG_AUTHOR);
 
+                        HashMap<String, String> map = new HashMap<String, String>();
+
+                        map.put(TAG_TITLE, title);
+                        map.put(TAG_CONTENT, content);
+                        map.put(TAG_AUTHOR, author);
 
 
 
+                        oslist.add(map);
 
+                        ListAdapter adapter = new SimpleAdapter(IndexActivity.this, oslist,
+                                R.layout.list_item,
+                                new String[] { TAG_TITLE,TAG_CONTENT, TAG_AUTHOR }, new int[] {
+                                R.id.title,R.id.content, R.id.author});
+                        setListAdapter(adapter);
 
                     }
                 } catch (JSONException e) {
                     e.printStackTrace();
                 }
-
-
-                intermediate = String.valueOf(articles.length());
-                //intermediate=response;
-                //intermediate=success;
-
             }
-            return response;
-        }
 
-        // onPostExecute displays the results of the AsyncTask.
-        @Override
-        protected void onPostExecute(String result) {
             progressBar.setVisibility(View.GONE);
-            textView.setText(intermediate);
+            //textView.setText(intermediate);
 
         }
 
---
+++ app/src/main/java/canul/android/SingleArticleActivity.java
@@ -0,0 +1,38 @@
+package canul.android;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SingleArticleActivity extends Activity {
+    // JSON node keys
+
+    private static final String TAG_TITLE = "title";
+    private static final String TAG_CONTENT = "content";
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_article);
+
+        // getting intent data
+        Intent in = getIntent();
+
+        // Get JSON values from previous intent
+        String title = in.getStringExtra(TAG_TITLE);
+        String content = in.getStringExtra(TAG_CONTENT);
+
+
+        // Displaying all values on the screen
+        TextView lbltitle = (TextView) findViewById(R.id.arttitle);
+        TextView lblcontent = (TextView) findViewById(R.id.artcontent);
+    ;
+
+        lbltitle.setText(title);
+        lblcontent.setText(content);
+
+
+    }
+
+}
